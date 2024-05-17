dc2d89c95758bc53dd3435b776dc135f54c9409a
Omar-will <omar.bengoudifa@outlook.fr>
2024-05-17 20:57:38 +0200

    add wp

--- src/components/Smoke.jsx
+++ src/components/Smoke.jsx
@@ -3,31 +3,31 @@ import * as THREE from 'three';
 
 const SmokeEffect = () => {
   useEffect(() => {
-    // Vérifier si une balise canvas existe déjà
+
     let canvasElement = document.getElementById('smoke-effect-canvas');
 
     if (!canvasElement) {
-      // Création de la scène
+
       const scene = new THREE.Scene();
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 100);
       const renderer = new THREE.WebGLRenderer({ alpha: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
-      renderer.domElement.id = 'smoke-effect-canvas'; // Définir l'ID pour la balise canvas
+      renderer.domElement.id = 'smoke-effect-canvas';
+      renderer.domElement.style.position = 'absolute'; 
+      renderer.domElement.style.top = 0; 
+      renderer.domElement.style.left = 0; 
+      renderer.domElement.style.width = '100%'; 
+      renderer.domElement.style.height = '100%'; 
       document.body.appendChild(renderer.domElement);
 
-      // Création de la texture de fumée
       const smokeTexture = new THREE.TextureLoader().load('assets/img/smoke.png');
       
-      // Création de la géométrie des particules de fumée
       const smokeGeometry = new THREE.PlaneGeometry(300, 300);
       
-      // Création du matériau des particules de fumée
       const smokeMaterial = new THREE.MeshLambertMaterial({ map: smokeTexture, opacity: 0.7, transparent: true });
       
-      // Tableau pour stocker les particules de fumée
       const smokeParticles = [];
 
-      // Création des particules de fumée
       for (let i = 0; i < 90; i++) {
         const smokeElement = new THREE.Mesh(smokeGeometry, smokeMaterial);
         smokeElement.scale.set(2, 2, 2);
@@ -37,35 +37,31 @@ const SmokeEffect = () => {
         smokeParticles.push(smokeElement);
       }
 
-      // Création d'une instance de Clock pour l'animation
       const clock = new THREE.Clock();
 
-      // Animation de l'effet visuel
       const animateSmoke = () => {
-        const delta = clock.getDelta(); // Obtention de la durée écoulée depuis la dernière frame
+        const delta = clock.getDelta();
         for (let i = 0; i < smokeParticles.length; i++) {
-          smokeParticles[i].rotation.z += delta * 0.2; // Rotation des particules
+          smokeParticles[i].rotation.z += delta * 0.2; 
         }
       };
 
-      // Fonction d'animation globale
       const animate = () => {
         requestAnimationFrame(animate);
-        animateSmoke(); // Appel de l'animation des particules de fumée
+        animateSmoke(); 
         renderer.render(scene, camera);
       };
 
-      animate(); // Lancement de l'animation
+      animate(); 
 
-      // Nettoyage lors de la désactivation du composant
       return () => {
-        // Nettoyez la scène et le rendu
+
         renderer.dispose();
       };
     }
   }, []);
 
-  return null; // Ne retourne rien dans le DOM, car le rendu est géré par Three.js
+  return null; 
 };
 
 export default SmokeEffect;
