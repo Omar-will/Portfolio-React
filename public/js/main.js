05f9d509eb4e1ac14fa791e9ab39738477f0734f
Omar-will <omar.bengoudifa@outlook.fr>
2024-05-31 22:29:04 +0200

    cv

--- public/js/main.js
+++ public/js/main.js
@@ -30,37 +30,3 @@ function scrollHeader() {
     
   }
   window.addEventListener("scroll", scrollActive);
-  
-  /*=============== LIGHT DARK THEME ===============*/
-  const themeButton = document.getElementById("theme-button");
-const lightTheme = "light-theme";
-const iconTheme = "bx-sun";
-
-const getCurrentTheme = () =>
-  document.body.classList.contains(lightTheme) ? "light" : "dark";
-const getCurrentIcon = () =>
-  themeButton.classList.contains(iconTheme) ? "bx bx-moon" : "bx bx-sun";
-
-const applyTheme = () => {
-  const selectedTheme = localStorage.getItem("selected-theme") || "light";
-  const selectedIcon = localStorage.getItem("selected-icon") || "bx bx-sun";
-
-  document.body.classList.toggle(lightTheme, selectedTheme === "light");
-  themeButton.classList.toggle(iconTheme, selectedIcon === "bx bx-sun");
-};
-
-applyTheme(); 
-
-themeButton.addEventListener("click", () => {
-  document.body.classList.toggle(lightTheme);
-  themeButton.classList.toggle(iconTheme);
-
-  const currentTheme = getCurrentTheme();
-  const currentIcon = getCurrentIcon();
-
-  localStorage.setItem("selected-theme", currentTheme);
-  localStorage.setItem("selected-icon", currentIcon);
-});
-
-  
-  
